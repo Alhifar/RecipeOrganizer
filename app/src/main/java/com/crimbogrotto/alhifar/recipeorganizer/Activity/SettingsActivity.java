0902e3a19d0b033bfcc09233c030212eb09d899d
Alhifar <alhifar@gmail.com>
2016-08-09 22:12:47 -0400

    Allow import and export of database for editing on PC

--- app/src/main/java/com/crimbogrotto/alhifar/recipeorganizer/Activity/SettingsActivity.java
+++ app/src/main/java/com/crimbogrotto/alhifar/recipeorganizer/Activity/SettingsActivity.java
@@ -1,7 +1,19 @@
 package com.crimbogrotto.alhifar.recipeorganizer.activity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.crimbogrotto.alhifar.recipeorganizer.R;
+import com.crimbogrotto.alhifar.recipeorganizer.db.RecipeContract;
+import com.crimbogrotto.alhifar.recipeorganizer.db.RecipeDbHelper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 
 /**
  * Created by Alhifar on 8/2/2016.
@@ -9,7 +21,70 @@ import android.support.v7.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void importClick(View view)
+    {
+        importDatabase();
+        ((TextView)findViewById(R.id.status_text)).setText("Imported database");
+    }
+
+    public void exportClick(View view)
+    {
+        exportDatabase();
+        ((TextView)findViewById(R.id.status_text)).setText("Exported database");
+    }
+
+    public void exportDatabase() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "//data//" + getPackageName() + "//databases//" + RecipeDbHelper.DATABASE_NAME;
+                String backupDBPath = RecipeDbHelper.DATABASE_NAME;
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+    public void importDatabase() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "//data//" + getPackageName() + "//databases//" + RecipeDbHelper.DATABASE_NAME;
+                String backupDBPath = RecipeDbHelper.DATABASE_NAME;
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(backupDB).getChannel();
+                    FileChannel dst = new FileOutputStream(currentDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
+
+
